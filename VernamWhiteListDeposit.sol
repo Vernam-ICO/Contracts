a56c51fb2cbe91c8222a4c41b4adc8f0077216d0
VenimirPetkov <venimirp@gmail.com>
2018-03-13 11:06:28 +0200

    Update

--- VernamWhiteListDeposit.sol
+++ VernamWhiteListDeposit.sol
@@ -1,18 +1,21 @@
+pragma solidity ^0.4.21;
+
 contract VernamWhiteListDeposit {
 	
 	address public benecifiary;
 	mapping (address => bool) public isWhiteList;
-	uint256 public constant depositAmount;
+	uint256 public constant depositAmount = 10000000000000000 wei;   // 0.01 ETH
 	
 	function VernamWhiteListDeposit(address _benecifiary) public {
 		benecifiary = _benecifiary;
-		depositAmount = 0.01 * 1 ethers;
 	}
 	
-	function() public paybale {
+	event WhiteListSuccess(address indexed _whiteListParticipant, uint256 _amount);
+	function() public payable {
 		require(msg.value == depositAmount);
 		require(!isWhiteList[msg.sender]);
 		benecifiary.transfer(msg.value);
 		isWhiteList[msg.sender] = true;
+		emit WhiteListSuccess(msg.sender, msg.value);
 	}
 }
