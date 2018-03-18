def3d87410a7a30fa8507015e682344822963377
Sevgin Mustafov <sevgin95@gmail.com>
2018-03-19 00:42:34 +0200

    Tokens Calculator added

--- Vernam Crowdsale.sol
+++ Vernam Crowdsale.sol
@@ -81,33 +81,45 @@ contract VernamCrowdSale is Ownable {
 	uint constant minimumContribution = 100 finney;
 	
 	uint constant public privatePreSaleDuration = 3 hours;
-	uint constant public privatePreSalePrice = 100000000000000 wei; //1 eth == 10 000
+	uint constant public privatePreSaleCapInWei = 100000000000000 wei; //1 eth == 10 000
 	uint constant public privatePreSaleTokens = 100000; // 100 000 tokens
 	
 	uint public privatePreSaleEnd;
 
 
 	uint constant public threeHotHoursDuration = 3 hours;
-	uint constant public threeHotHoursPrice = 100000000000000 wei; //1 eth == 10 000
+	uint constant public threeHotHoursCapInWei = 100000000000000 wei; //1 eth == 10 000
 	uint constant public threeHotHoursTokens = 100000; // 100 000 tokens
+	
+	uint tokensPerEthInTHH = 1 ether / threeHotHoursCapInWei;
 
 	uint public threeHotHoursEnd;
 	
 	uint constant public firstStageDuration = 24 hours;
-	uint constant public firstStagePrice = 200000000000000 wei;    //1 eth == 5000
+	uint constant public firstStageCapInWei = 200000000000000 wei;    //1 eth == 5000
 	uint constant public firstStageTokens = 100000; // 100 000 tokens  //maybe not constant because we must recalculate if previous have remainig
 
+    uint tokensPerEthInFirstStage = 1 ether / firstStageCapInWei;
+    
 	uint public firstStageEnd;
 	
 	uint constant public secondStageDuration = 6 days;
-	uint constant public secondStagePrice = 400000000000000 wei; //1 eth == 2500
+	uint constant public secondStageCapInWei = 400000000000000 wei; //1 eth == 2500
 	uint constant public secondStageTokens = 100000; // 100 000 tokens       //maybe not constant because we must recalculate if previous have remainig
 
+    uint tokensPerEthInSecondStage = 1 ether / secondStageCapInWei;
+    
 	uint public secondStageEnd;
 	
 	uint constant public thirdStageDuration = 26 days;
-	uint constant public thirdStagePrice = 600000000000000 wei;          //1 eth == 1500
-	uint constant public thirdStageDiscountPrice = 800000000000000 wei; //1 eth == 1250
+	uint constant public thirdStageCapInWei = 600000000000000 wei;          //1 eth == 1500
+	
+	uint tokensPerEthInThirdStage = 1 ether / thirdStageCapInWei;
+	
+	uint constant public thirdStageDiscountCapInWei = 800000000000000 wei; //1 eth == 1250
+	
+	uint tokensPerEthInThirdDiscountStage = 1 ether / thirdStageDiscountCapInWei;
+	
 	uint constant public thirdStageTokens = 100000; // 100 000 tokens //maybe not constant because we must recalculate if previous have remainig
 	uint public thirdStageEnd;
 	
@@ -155,52 +167,81 @@ contract VernamCrowdSale is Ownable {
 	
 	function buyTokens(address _participant, uint256 _weiAmount) public payable returns(bool) {
 		require(_weiAmount > minimumContribution); // if value is smaller than most expensive stage price will count 0 tokens 
-		uint256 _price = getTokenPrice(_weiAmount);
-		writeOrder(_participant,_weiAmount,_price);
+		
+		//uint256 _price = getTokenPrice(_weiAmount);
+		
+		// We will give it _participant also to mint tokens to him
+		calculateAndCreateTokens(_weiAmount);
+        
+        //require(totalSoldTokens > cap);
+		
+		// _price - we will fix it
+		// writeOrder(_participant, _weiAmount, _price);
 	}
 	
-    function getTokenPrice(uint256 _weiAmount) public view returns (uint256) {
+	function calculateAndCreateTokens(uint256 weiAmount) public {
 		
-		if(block.timestamp < privatePreSaleEnd && totalSoldTokens < privatePreSaleTokens){
+		// The private presale operations must be in another functions 
+		/*if(block.timestamp < privatePreSaleEnd && totalSoldTokens < privatePreSaleTokens){
 			return privatePreSalePrice;
+		}*/
+		
+		// Safe math will be used 
+		if(block.timestamp < threeHotHoursEnd && totalSoldTokens < threeHotHoursEnd) {
+		    tokensCalculator(weiAmount, tokensPerEthInTHH, tokensPerEthInFirstStage, threeHotHoursCapInWei);
 		}
 		
-		if(block.timestamp < threeHotHoursEnd && totalSoldTokens < threeHotHoursTokens){
-			return threeHotHoursPrice;
+		if(block.timestamp < firstStageEnd && totalSoldTokens < firstStageTokens) {
+		    tokensCalculator(weiAmount, tokensPerEthInFirstStage, tokensPerEthInSecondStage, firstStageCapInWei);
 		}
 		
-		if(block.timestamp < firstStageEnd && totalSoldTokens < firstStageTokens){
-			return firstStagePrice;
+		if(block.timestamp < secondStageEnd && totalSoldTokens < secondStageTokens) {
+			tokensCalculator(weiAmount, tokensPerEthInSecondStage, tokensPerEthInThirdStage, secondStageCapInWei);
 		}
 		
-		if(block.timestamp < secondStageEnd && totalSoldTokens < secondStageTokens){
-			return secondStagePrice;
-		}
-		
-		if(block.timestamp < thirdStageEnd && totalSoldTokens < thirdStageTokens && _weiAmount > FIFTEEN_ETHERS){
-			return thirdStageDiscountPrice;
+		if(block.timestamp < thirdStageEnd && totalSoldTokens < thirdStageTokens && weiAmount > FIFTEEN_ETHERS) {
+			tokensCalculator(weiAmount, tokensPerEthInThirdDiscountStage, tokensPerEthInThirdDiscountStage, thirdStageDiscountCapInWei);
 		}
 		
 		if(block.timestamp < thirdStageEnd && totalSoldTokens < thirdStageTokens){
-			return thirdStagePrice;
+		   tokensCalculator(weiAmount, tokensPerEthInThirdStage, tokensPerEthInThirdStage, thirdStageCapInWei);
 		}
-		revert();
 	}
 	
+	uint256 totalContributedWei;
+	function tokensCalculator(uint256 weiAmount, uint256 currentLevelPrice, uint256 nextLevelPrice, uint256 currentLevelCap) internal {
+	    uint currentAmountInWei = 0;
+		uint remainingAmountInWei = 0;
+		uint amount = 0; 
+		if(weiAmount + totalContributedWei > currentLevelCap) {
+		    remainingAmountInWei = (weiAmount + totalContributedWei) - currentLevelCap;
+		    currentAmountInWei = weiAmount - remainingAmountInWei;
+
+            amount = currentAmountInWei.mul(currentLevelPrice);
+            amount += remainingAmountInWei.mul(nextLevelPrice);
+	    } else {
+	        amount = weiAmount.mul(currentLevelPrice);
+	    }
+		
+		// Mint tokens token.mint(amount) 
+		totalSoldTokens = totalSoldTokens.add(amount);
+		totalContributedWei += weiAmount;
+	}
+    
 	function writeOrder(address _participant, uint256 _weiAmount, uint _price) internal {
-		if(_price == privatePreSalePrice) {
+		if(_price == privatePreSaleCapInWei) {
 			OrdersDetail[_participant].privatePresaleTokens = OrdersDetail[_participant].privatePresaleTokens.add(_weiAmount.div(_price));
 			OrdersDetail[_participant].privatePresaleWEI = OrdersDetail[_participant].privatePresaleWEI.add(_weiAmount);
-		}else if (_price == threeHotHoursPrice) {
+		}else if (_price == totalContributedWei) {
 			OrdersDetail[_participant].threeHotHoursTokens = OrdersDetail[_participant].threeHotHoursTokens.add(_weiAmount.div(_price));
 			OrdersDetail[_participant].threeHotHoursWEI = OrdersDetail[_participant].threeHotHoursWEI.add(_weiAmount);
-		}else if (_price == firstStagePrice) {
+		}else if (_price == firstStageCapInWei) {
 			OrdersDetail[_participant].firstStageTokens = OrdersDetail[_participant].firstStageTokens.add(_weiAmount.div(_price));
 			OrdersDetail[_participant].firstStageWEI = OrdersDetail[_participant].firstStageWEI.add(_weiAmount);
-		}else if (_price == secondStagePrice) {
+		}else if (_price == secondStageCapInWei) {
 			OrdersDetail[_participant].secondStageTokens = OrdersDetail[_participant].secondStageTokens.add(_weiAmount.div(_price));
 			OrdersDetail[_participant].secondStageWEI = OrdersDetail[_participant].secondStageWEI.add(_weiAmount);
-		}else if (_price == thirdStageDiscountPrice) {
+		}else if (_price == thirdStageDiscountCapInWei) {
 			OrdersDetail[_participant].thirdStageWithDiscountTokens = OrdersDetail[_participant].thirdStageWithDiscountTokens.add(_weiAmount.div(_price));
 			OrdersDetail[_participant].thirdStageWithDiscountWEI = OrdersDetail[_participant].thirdStageWithDiscountWEI.add(_weiAmount);
 		}else {
@@ -209,34 +250,3 @@ contract VernamCrowdSale is Ownable {
 		}
 	} 
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
